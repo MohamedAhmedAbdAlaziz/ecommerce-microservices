94e283f26812c224359a321ea0897c33cc3c3d6a
MoSallam98 <mohamed.abdelaziz@techspear.net>
2024-03-02 11:23:21 +0200

    add basket api

---
+++ Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get;set; }
+        public List<ShoppingCartItem> Items { get; set; }= new List<ShoppingCartItem>();
+
+        public ShoppingCart( )
+        {
+
+        }
+        public ShoppingCart( string UserName)
+        {
+
+            this.UserName = UserName;
+        }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totoalPrice=0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totoalPrice= item.Price *item.Quantity;
+                }
+                return totoalPrice;
+            }
+
+        }
+    }
+}
--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ 
+builder.Services.AddSwaggerGen(
+    c => c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Basket.API", Version = "v1" }));
 
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basket.API v1"));
 }
 
 app.UseAuthorization();
